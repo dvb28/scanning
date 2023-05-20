8a0f446397246a80df1133df3aeb33e965a152a7
dvb28 <vietbao2k2@gmail.com>
2023-05-20 13:14:45 +0700

    13:13 20/5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
+
 export default function Home() {
   const route = useRouter();
 
   useEffect(() => {
-    route.push('/auth/login');
+    route.push('/auth/login'); 
   }, [route]);
 
   return (
